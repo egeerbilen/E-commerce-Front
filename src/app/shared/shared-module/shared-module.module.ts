94a1927e0ba6275285a892306d6bae5d004a5237
DESKTOP-9AE13CG\Ege Erbilen <gegeerb2635@gmail.com>
2024-07-12 13:09:52 +0300

    home pag eto angular material

--- src/app/shared/shared-module/shared-module.module.ts
+++ src/app/shared/shared-module/shared-module.module.ts
@@ -2,7 +2,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { HighlightCategoryDirective } from 'src/app/helpers/highlight-category/highlight-category.directive';
@@ -24,6 +30,12 @@ import { LoadingPageService } from '../services/loading-page/loading-page.servic
     ReactiveFormsModule,
     HttpClientModule,
     MatSnackBarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
     BrowserAnimationsModule
   ],
   declarations: [LoadingPageComponent, ByteToImagePipe, ModalHelperComponent, HighlightCategoryDirective],
@@ -38,6 +50,12 @@ import { LoadingPageService } from '../services/loading-page/loading-page.servic
     LoadingPageComponent,
     ModalHelperComponent,
     MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
     HighlightCategoryDirective
   ]
