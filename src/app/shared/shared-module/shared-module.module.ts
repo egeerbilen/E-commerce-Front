1a81344df7546b0662340b1187367f6ff472dd66
DESKTOP-9AE13CG\Ege Erbilen <gegeerb2635@gmail.com>
2024-07-12 14:19:58 +0300

    .

--- src/app/shared/shared-module/shared-module.module.ts
+++ src/app/shared/shared-module/shared-module.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -34,6 +36,8 @@ import { LoadingPageService } from '../services/loading-page/loading-page.servic
     MatButtonModule,
     MatInputModule,
     MatListModule,
+    MatSelectModule,
+    MatOptionModule,
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
@@ -46,6 +50,8 @@ import { LoadingPageService } from '../services/loading-page/loading-page.servic
     CommonModule,
     RouterModule,
     FormsModule,
+    MatSelectModule,
+    MatOptionModule,
     ReactiveFormsModule,
     HttpClientModule,
     ByteToImagePipe,
