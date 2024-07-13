07c704c01ab3288245c0f2bf0ed0a33ceb7d30bf
DESKTOP-9AE13CG\Ege Erbilen <gegeerb2635@gmail.com>
2024-07-14 02:13:16 +0300

    .

--- src/app/modules/product-management/product-management.routing.ts
+++ src/app/modules/product-management/product-management.routing.ts
@@ -8,22 +8,18 @@ const routes: Routes = [
   {
     path: '',
     component: ProductManagementComponent,
-    pathMatch: 'full',
     title: 'Product Management',
     resolve: { resolvedData: ProductManagementDataResolveService },
     children: [
       {
-        pathMatch: 'full',
         path: urlEnums.updateProduct + '/:id',
         loadChildren: () => import('./update-product/update-product.module').then((m) => m.UpdateProductModule)
       },
       {
-        pathMatch: 'full',
         path: urlEnums.categoryManagement,
         loadChildren: () => import('./category-management/category-management.module').then((m) => m.CategoryManagementModule)
       },
       {
-        pathMatch: 'full',
         path: urlEnums.addProduct,
         loadChildren: () => import('./add-product/add-product.module').then((m) => m.AddProductModule)
       }
