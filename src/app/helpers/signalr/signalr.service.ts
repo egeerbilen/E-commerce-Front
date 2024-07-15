5deb14461baf264c2cb8268279f3384049142707
DESKTOP-9AE13CG\Ege Erbilen <gegeerb2635@gmail.com>
2024-07-15 12:21:10 +0300

    singr added

---
+++ src/app/helpers/signalr/signalr.service.ts
@@ -0,0 +1,46 @@
+// src/app/services/signalr.service.ts
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalrService {
+  private _hubConnection: signalR.HubConnection;
+
+  /**
+   * Constructor.
+   */
+  constructor() {
+    this._hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7095/chatHub') // API URL'nizi buraya ekleyin
+      .build();
+
+    this._startConnection();
+  }
+
+  /**
+   * AddReceiveMessageListener.
+   * @param callback Callback.
+   */
+  public addReceiveMessageListener(callback: (user: string, message: string) => void): void {
+    this._hubConnection.on('ReceiveMessage', callback);
+  }
+  /**
+   * SendMessage.
+   * @param user User.
+   * @param message Message.
+   */
+  public sendMessage(user: string, message: string): void {
+    this._hubConnection.invoke('SendMessage', user, message).catch((err) => console.error(err));
+  }
+  /**
+   * StartConnection.
+   */
+  private _startConnection(): void {
+    this._hubConnection
+      .start()
+      .then(() => console.log('SignalR connection started'))
+      .catch((err) => console.log('Error while starting SignalR connection: ' + err));
+  }
+}
